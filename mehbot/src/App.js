b4118f48901c83e97c203ec018a758f7984e82fa
NotSoToxic <sarthak.joshi52@gmail.com>
2023-10-16 09:34:24 +0530

    Update

--- mehbot/src/App.js
+++ mehbot/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [responses, setResponses] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [diagnosis, setDiagnosis] = useState(null);
+  const questions = [
+    "Do you often feel sad or hopeless?",
+    "Do you have trouble concentrating?",
+    "Have you experienced a loss of interest in things you once enjoyed?",
+    "Do you have trouble sleeping, either sleeping too much or too little?",
+    "Have you had thoughts of self-harm or suicide?",
+    // Add more questions as needed
+  ];
+
+  const handleResponse = async (response) => {
+    setResponses([...responses, response]);
+    if (currentQuestionIndex + 1 < questions.length) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      const diagnosisResponse = await fetch('http://127.0.0.1:5000', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ responses: responses }),
+      });
+      const diagnosisData = await diagnosisResponse.json();
+      setDiagnosis(diagnosisData.diagnosis);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {diagnosis ? (
+          <div>
+            <p>Diagnosis: {diagnosis}</p>
+          </div>
+        ) : (
+          <div>
+            <p>Hello! I'm here to help you evaluate your mental health.</p>
+            <p>{questions[currentQuestionIndex]}</p>
+            <button onClick={() => handleResponse('yes')}>Yes</button>
+            <button onClick={() => handleResponse('no')}>No</button>
+          </div>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ mehbot/src/app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+class MentalHealthChatbot:
+    def __init__(self):
+        self.responses = []
+        self.questions = [
+            "How often do you feel sad or hopeless?",
+            "Do you have trouble concentrating?",
+            "Have you experienced a loss of interest in things you once enjoyed?",
+            "Have you had thoughts of self-harm or suicide?",
+            # Add more questions as needed
+        ]
+        self.diagnoses = {
+            0: "You seem to be in good mental health.",
+            1: "You might be experiencing mild stress.",
+            2: "You may be showing signs of generalized anxiety disorder (GAD).",
+            3: "You might be suffering from mild to moderate depression.",
+            4: "You may be dealing with severe depression. Please seek professional help immediately.",
+        }
+
+    def get_evaluation(self):
+        score = 0
+        for response in self.responses:
+            if re.search(r'\b(?:yes|often|always)\b', response, re.IGNORECASE):
+                score += 1
+        return score
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    if request.method == 'POST':
+        data = request.get_json()
+        chatbot = MentalHealthChatbot()
+        chatbot.responses = data['responses']
+        evaluation = chatbot.get_evaluation()
+        if evaluation in chatbot.diagnoses:
+            return jsonify({'evaluation': evaluation, 'diagnosis': chatbot.diagnoses[evaluation]})
+        else:
+            return jsonify({'evaluation': evaluation, 'diagnosis': "It is recommended to consult a mental health professional for an accurate diagnosis."})
+
+if __name__ == '__main__':
+    app.run(debug=True)
